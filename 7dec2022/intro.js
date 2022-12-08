5dc7ac3c6b34822f41d8c384452328e5965dfdb9
keerthana <vijayakannanpandian@gmail.com>
2022-12-08 15:38:06 +0530

    Initial commit

---
+++ 7dec2022/intro.js
@@ -0,0 +1,13 @@
+let name1="vimal"
+
+    const name2 = "iamneo"
+    
+    function newName(){
+        var name0 ="skct"
+        let name1 ="vimal"
+        const name2="iamneo"
+        console.log(name0,name1,name2)
+    }
+
+newName();
+console.log(name0,name1,name2)
